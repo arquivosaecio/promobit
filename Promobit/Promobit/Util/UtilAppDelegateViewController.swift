b9f945843b9d567cddf48166b54f12c3e8452cec
Professor Aecio <arquivosaecio@yahoo.com.br>
2021-02-06 14:12:57 -0300

    Adição do ícone ao projeto

--- Promobit/Promobit/Util/UtilAppDelegateViewController.swift
+++ Promobit/Promobit/Util/UtilAppDelegateViewController.swift
@@ -3,11 +3,15 @@
 //  Promobit
 //
 //  Created by Master Mac on 06/02/21.
+//  Autor : Professor Aécio
 //
 
 import UIKit
 
 class UtilAppDelegateViewController: UIViewController {
+    
+    // Apontamento Singleton para classe AppDelegate.swift onde os objetos ficarão
+    let app = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +19,9 @@ class UtilAppDelegateViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Interrupção: ao tocar na tela ocultar o teclado
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-    */
 
 }
