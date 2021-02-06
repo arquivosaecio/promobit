cc56854d9a5b2cbeb97a1844aa0b3432a2226401
Professor Aecio <arquivosaecio@yahoo.com.br>
2021-02-06 14:32:35 -0300

    Adição arquivo de controle de conexão a internet

---
+++ Promobit/Promobit/Util/UtilInternet.swift
@@ -0,0 +1,16 @@
+//
+//  UtilInternet.swift
+//  Promobit
+//
+//  Created by Master Mac on 06/02/21.
+//  Autor : Professor Aécio
+//
+
+import UIKit
+
+class UtilInternet: NSObject {
+
+    // Utilitário cuja função é relatar se o app tem conexão
+    var possuiConexao : Bool = true
+    
+}
