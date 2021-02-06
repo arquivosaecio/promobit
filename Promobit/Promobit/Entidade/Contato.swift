2ec275f29a4340d2c7f98d0dc100ac4520082267
Professor Aecio <arquivosaecio@yahoo.com.br>
2021-02-06 14:26:58 -0300

    Adicao entidade Contato.swift

---
+++ Promobit/Promobit/Entidade/Contato.swift
@@ -0,0 +1,23 @@
+//
+//  Contato.swift
+//  Promobit
+//
+//  Created by Master Mac on 06/02/21.
+//  Autor : Professor Aécio
+//
+
+import UIKit
+
+class Contato: NSObject {
+    
+    var id = ""
+    var createdAt = ""
+    var name = ""
+    var avatar = ""
+    var company = ""
+    var email = ""
+    var phone = ""
+    var website = ""
+    var custom_note = ""
+
+}
